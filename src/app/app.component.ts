84590e38464a7316dd89720267d39ad16562aa2c
DESKTOP-VNUQAL2\CES <congotech17@gmail.com>
2024-12-09 15:55:42 +0100

    Template : Ajouts des pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<h1>Welcome to {{pokemons[1]}}</h1>`,
+  imports: [RouterOutlet, NgIf, NgFor],
+  templateUrl:'app.component.html',
   styles: [],
 })
-export class AppComponent {
-  pokemons = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+export class AppComponent implements OnInit{
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined
+
+  ngOnInit(): void {
+      //console.table(this.pokemonList)
+      //this.selectPokemon(this.pokemonList[0])
+  }
+
+  selectPokemon(pokemonId: string){
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == Number(pokemonId));
+    if(pokemon){
+    console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
+    this.pokemonSelected = pokemon;
+    } else{
+    console.log(`Vous avez cliqué sur le pokemon qui n'existe pas dans le tableau`);
+    this.pokemonSelected = pokemon;
+    }
+  }
 }
